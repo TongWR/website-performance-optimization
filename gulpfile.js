b3b130e72cebf989fc3c70b1fe16a53952ef3675
Tong W. Ratanapan <tong.worapol@gmail.com>
2016-11-14 15:18:44 +0700

    fix: Pizza image now appears in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
-gulp.task('hello', function() {
-  console.log('Gulp again?');
+gulp.task('default', ['html', 'css', 'js', 'images', 'pizza-images'], function() {
+
 });
 
 gulp.task('html', function() {
@@ -17,4 +17,14 @@ gulp.task('css', function() {
 gulp.task('js', function() {
   return gulp.src('src/js/*.js')
     .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/img/*')
+    .pipe(gulp.dest('dist/img'));
+})
+
+gulp.task('pizza-images', function() {
+  return gulp.src('src/views/images/*')
+    .pipe(gulp.dest('dist/views/images'));
 });
