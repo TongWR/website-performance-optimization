2fb4978dff639f6bd3c7b21dac6c90d0be586532
Tong W. Ratanapan <tong.worapol@gmail.com>
2016-11-16 22:59:04 +0700

    Reduce pizza resizing time by removing layout trashing

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
-var inlinesource = require('gulp-inline-source');
+var htmlMin = require('gulp-htmlmin');
+var inlineSource = require('gulp-inline-source');
+var imageMin = require('gulp-imagemin');
+var del = require('del');
+var runSequence = require('run-sequence');
 
-gulp.task('default', ['html', 'css', 'js', 'assets'], function() {
-
+gulp.task('default', function() {
+  runSequence('clean:dist', ['html', 'css', 'js', 'assets']);
 });
 
 gulp.task('html', function() {
   return gulp.src('src/index.html')
-    .pipe(inlinesource())
-    .pipe(htmlmin({collapseWhitespace: true, removeComments: true, minifyJS: true}))
+    .pipe(inlineSource())
+    .pipe(htmlMin({collapseWhitespace: true, removeComments: true, minifyJS: true}))
     .pipe(gulp.dest('dist'));
 });
 
@@ -29,11 +32,33 @@ gulp.task('assets', ['images', 'pizza-images', 'fonts'], function() {
 
 gulp.task('images', function() {
   return gulp.src('src/img/*')
+    .pipe(imageMin())
     .pipe(gulp.dest('dist/img'));
 })
 
+gulp.task('pizza', function() {
+  return gulp.src('src/views/**/*')
+    .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('pizza-html', function() {
+  return gulp.src('src/views/pizza.html')
+    .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('pizza-css', function() {
+  return gulp.src('src/views/css/*.css')
+    .pipe(gulp.dest('dist/views/css'));
+});
+
+gulp.task('pizza-js', function() {
+  return gulp.src('src/views/js/*.js')
+    .pipe(gulp.dest('dist/views/js'))
+});
+
 gulp.task('pizza-images', function() {
   return gulp.src('src/views/images/*')
+    .pipe(imageMin())
     .pipe(gulp.dest('dist/views/images'));
 });
 
@@ -41,3 +66,7 @@ gulp.task('fonts', function() {
   return gulp.src('src/fonts/*')
     .pipe(gulp.dest('dist/fonts'));
 });
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
